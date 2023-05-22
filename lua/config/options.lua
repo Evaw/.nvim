e4abb8b51a7be3d971bc008eebff22ade9649325
jorgejaramillo <engjorge@outlook.com>
2023-05-21 22:02:02 -0700

    visible blank spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,12 @@
 -- Add any additional options here
 -- example from https://www.youtube.com/watch?v=N93cTbtLCIM
 vim.opt.winbar = "%=%m %f"
+vim.opt.listchars = {
+  tab = "▸ ",
+  trail = "·",
+  precedes = "←",
+  extends = "→",
+  nbsp = "·",
+  eol = "↲",
+  space = "␣",
+}
